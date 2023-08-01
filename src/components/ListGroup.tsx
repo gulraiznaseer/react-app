8493b42851c0dbb10c189e1e341c8063a81555dc
gulraiznaseer <gulraiz.naseer@amroodlabs.com>
2023-08-01 14:56:08 +0500

    Handle onClick functionality by function

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,15 +1,17 @@
 function ListGroup() {
   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-  items = [];
-
+  // items = [];
+  const handleClick = (event: MouseEvent) => (console.log(event))
   return (
     <>
       <h1>List</h1>
-      { (items.length === 0) && <p> No item Found </p> }
+      {items.length === 0 && <p> No item Found </p>}
 
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item}>{item}</li>
+        {items.map((item, index) => (
+          <li className="item-list-group" key={item} onClick={handleClick }>
+            {item}
+          </li>
         ))}
       </ul>
     </>
