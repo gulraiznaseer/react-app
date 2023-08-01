17bc5d9fcc89cf107c6bcd1b42ab45740eff2373
gulraiznaseer <gulraiz.naseer@amroodlabs.com>
2023-08-01 16:50:39 +0500

    Send function via prop

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import ListGroup from './components/ListGroup';
 
 function App() {
   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  const handleSelectItem = (item: string) => {console.log({item})}
 
-  return <div><ListGroup items={items} heading="Cities"/></div>
-
+  return <div><ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem} /></div>
 }
 
 export default App
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -2,9 +2,10 @@ import { MouseEvent, useState } from "react";
 interface Props {
   items: string[];
   heading: string;
+  onSelectItem: (item: string) => void;
 }
 
-function ListGroup({items, heading}: Props) {
+function ListGroup({ items, heading, onSelectItem }: Props) {
   const [selectedIndex, setSelectedIndex] = useState(-1);
 
   return (
@@ -14,7 +15,18 @@ function ListGroup({items, heading}: Props) {
 
       <ul className="list-group">
         {items.map((item, index) => (
-          <li className={ selectedIndex === index ? "list-group-item active" : "list-group-item"} key={item} onClick= { () => setSelectedIndex(index) }>
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index)
+              onSelectItem(item)
+            }}
+          >
             {item}
           </li>
         ))}
