0419e02c9970c4a2c890f7e986d38282cf64b534
gulraiznaseer <gulraiz.naseer@amroodlabs.com>
2023-08-01 17:05:00 +0500

    fixed the issue which is printing the whole object rather data single value

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import ListGroup from './components/ListGroup';
 
 function App() {
   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-  const handleSelectItem = (item: string) => {console.log({item})}
+  const handleSelectItem = (item: string) => {console.log(item)}
 
   return <div><ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem} /></div>
 }
