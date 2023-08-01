8d903987e4213e1fb5c75748ffcae819471b581b
gulraiznaseer <gulraiz.naseer@amroodlabs.com>
2023-08-01 13:55:50 +0500

    Add Heading and display dynamically

--- src/Message.tsx
+++ src/Message.tsx
@@ -1,5 +1,9 @@
 function Message(){
-    return <h1>Hello world!</h1>;
+  const name = "Gulraiz"
+  // const name = "" this is an empty/false condition, will print hello world
+  if (name)
+    return <h1>Hello {name}!</h1>;
+  return <h1>Hello World!</h1>;
 }
 
 export default Message;
