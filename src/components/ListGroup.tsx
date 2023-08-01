ce3fdad2eadb2be0501095e9caf34c36bf00e60a
gulraiznaseer <gulraiz.naseer@amroodlabs.com>
2023-08-01 14:37:37 +0500

    Add unique key for each listing

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,17 +1,20 @@
 function ListGroup() {
-  const items = [
-    "New York",
-    "San Francisco",
-    "Tokyo",
-    "London",
-    "Paris",
-  ]
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  items = [];
+  if (items.length === 0)
+    return (
+      <>
+        <h1>List</h1> <p>No items Found!</p>
+      </>
+    );
 
   return (
     <>
       <h1>List</h1>
       <ul className="list-group">
-        { items.map(item => <li key={item}>{item}</li>)}
+        {items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
       </ul>
     </>
   );
