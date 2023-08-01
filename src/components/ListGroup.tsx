8f821682907a833a9f7fc2abad3488a0ffd04bcc
gulraiznaseer <gulraiz.naseer@amroodlabs.com>
2023-08-01 14:40:06 +0500

    Refctor code

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,16 +1,12 @@
 function ListGroup() {
   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
   items = [];
-  if (items.length === 0)
-    return (
-      <>
-        <h1>List</h1> <p>No items Found!</p>
-      </>
-    );
 
   return (
     <>
       <h1>List</h1>
+      { (items.length === 0) && <p> No item Found </p> }
+
       <ul className="list-group">
         {items.map((item) => (
           <li key={item}>{item}</li>
