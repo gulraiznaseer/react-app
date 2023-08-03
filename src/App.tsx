42578baff86ee02294b4b9d230cb0977ee6b1399
gulraiznaseer <gulraiz.naseer@amroodlabs.com>
2023-08-03 15:54:40 +0500

    Dissmissable Alert added

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
 import ListGroup from "./components/ListGroup";
 import Alert from "./components/Alert";
 import Button from "./components/Button";
+import { useState } from "react";
 
 function App() {
   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  const [alertVisible, setAlertVisibility] = useState(false);
   const handleSelectItem = (item: string) => {
     console.log(item);
   };
 
   return (
     <div>
-      <Button color="danger" onClick={() => console.log("Clicked")}>
+      {alertVisible && (
+        <Alert onClose={() => setAlertVisibility(false)}> My Alert! </Alert>
+      )}
+      <Button color="danger" onClick={() => setAlertVisibility(true)}>
         Click Buton
       </Button>
     </div>
